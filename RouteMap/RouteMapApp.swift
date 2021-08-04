05e0553db4ee3e42e5afea86377b4e8626fe9d26
Jack Finnis <finnis.j@etoncollege.org.uk>
2021-08-04 13:50:52 +0100

    Initial Commit

---
+++ RouteMap/RouteMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  RouteMapApp.swift
+//  RouteMap
+//
+//  Created by William Finnis on 04/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct RouteMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
